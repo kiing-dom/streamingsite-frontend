5db76c0750f1d2fee7dc877fc23d9e86e57a441c
Dominion Gbadamosi <worshipgbadamosi@live.com>
2024-08-10 23:24:48 +0100

    added protected routes for user and admin

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,8 @@ import Login from './components/auth/Login';
 import Register from './components/auth/Register';
 import { Toaster } from 'react-hot-toast';
 import Landing from './components/landing/Landing';
+import PrivateRoute from './components/routes/PrivateRoute';
+import AdminDashboard from './components/home/AdminDashboard';
 
 function App() {
   return (
@@ -18,7 +20,21 @@ function App() {
             <Route path='/' element={<Landing />} />
             <Route path='/login' element={<Login />} />
             <Route path='/register' element={<Register />} />
-            <Route path='/home' element={<Home />} />
+            <Route path='/home'
+              element={
+                <PrivateRoute>
+                  <Home />
+                </PrivateRoute>
+              }
+            />
+            <Route
+              path='/admin_dashboard'
+              element={
+                <PrivateRoute>
+                  <AdminDashboard />
+                </PrivateRoute>
+              }
+            />
           </Routes>
         </div>
       </Router>
---
+++ src/components/home/AdminDashboard.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function AdminDashboard() {
+  return (
+    <div className='flex justify-center items-center h-[84vh]'>
+      Admin Dashboard
+    </div>
+  )
+}
